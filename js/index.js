062cbb4ebf7485450abb609718cb092850a708f3
Shivam Saheb <gryfphen@gmail.com>
2019-01-09 00:27:34 -0500

    sound

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-let regal = [4, 5, 6];
-
-console.log(regal);
-
-regal.push(46);
-
-console.log(regal);
-
-console.log('5'+5)
+window.onload = function() {
+    document.getElementById("my_audio").play();
+}

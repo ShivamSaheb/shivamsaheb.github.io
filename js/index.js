7ff51052ac1969245bbe0b7417466a9ad97a942e
Shivam Saheb <gryfphen@gmail.com>
2025-08-15 01:52:32 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-// Function to remove background class (used for visual effect)
+// Function to remove background class (visual effect)
 function about() {
     document.getElementsByClassName("bg")[0].setAttribute("class", "")
 }
 
-// This script triggers the 5-second redirect if user clicked the logo from About page
+// Redirect countdown only if user clicked logo from About page
 window.onload = function() {
     const params = new URLSearchParams(window.location.search);
     const fromAbout = params.get("from") === "about";
 
     if (fromAbout) {
-        // Redirect to the root redirect page for countdown
         window.location.href = "../redirect.html?from=about";
     }
 }

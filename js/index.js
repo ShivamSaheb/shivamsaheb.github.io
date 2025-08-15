c397fd898b849b1b1000e5072ba6101ee8e0ba6a
Shivam Saheb <gryfphen@gmail.com>
2025-08-15 02:36:58 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-// Function to remove background class
-function about() {
-    document.getElementsByClassName("bg")[0].setAttribute("class", "");
-}
 
-// 5-second redirect if logo clicked from About page
-window.onload = function() {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get("from") === "about") {
-        window.location.href = "../redirect.html?from=about";
-    }
-}

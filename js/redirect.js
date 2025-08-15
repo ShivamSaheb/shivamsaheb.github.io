85bc2684b1edf450b3a36efc7ad96ad5c74ff9aa
Shivam Saheb <gryfphen@gmail.com>
2025-08-15 01:41:01 -0400

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,12 +1,16 @@
+// Countdown time in seconds
 let countdownTime = 5;
+
+// Reference to countdown element
 const countdownElement = document.getElementById("countdown");
 
+// Update countdown every second
 const timer = setInterval(() => {
-  countdownTime--;
-  countdownElement.textContent = countdownTime;
+    countdownTime--;
+    countdownElement.textContent = countdownTime;
 
-  if (countdownTime <= 0) {
-    clearInterval(timer);
-    window.location.href = "html/index.html";
-  }
+    if (countdownTime <= 0) {
+        clearInterval(timer);
+        window.location.href = "html/index.html"; // Redirect to main site
+    }
 }, 1000);
